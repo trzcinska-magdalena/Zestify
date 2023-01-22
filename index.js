0926f8d4399bd4f3167594ec8f8d5a91ce91adbe
Magdalena Trzcińska <trzcinskamagdalena01@gmail.com>
2023-01-22 19:25:51 +0100

    code upgrade

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function openMenu() {
-    let selectors = document.querySelectorAll("header nav .navigation li.open~li");
+    let selectors = document.querySelectorAll("header .container .navigation .navigation__item--open~li");
 
     for(let i=0; i<selectors.length; i++) {
         console.log(selectors[i]);
-        selectors[i].classList.toggle("visible-mobile");
-        selectors[i].classList.toggle("hidden-mobile");
+        selectors[i].classList.toggle("navigation__item--visible-mobile");
+        selectors[i].classList.toggle("navigation__item--hidden-mobile");
         
     }
 }
 
 let open = document.getElementById("open");
 open.addEventListener('click', openMenu);
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
